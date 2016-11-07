8ae6e5a544b746b69ebaa3d6470ed917e6d1a69d
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-07 16:04:11 +0100

    new jog component

--- client/js/components/jogs/NewJog.js
+++ client/js/components/jogs/NewJog.js
@@ -1,15 +1,67 @@
 import React from "react";
+import { isEmpty } from "lodash";
 import styles from "./new_jog.scss";
 
 class NewJog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: props.errors || {}
+    }
+  }
+
   submitJog(e) {
     e.preventDefault();
-    // TODO format values
-    this.props.onSubmitJog({
+
+    let params = {
       date: this.refs.date.value,
-      distance: this.refs.distance.value,
-      duration: this.refs.duration.value,
-    })
+      distance: this.formattedDistance(),
+      duration: this.formattedDuration(),
+    }
+
+    let errors = this.validate(params)
+    if(isEmpty(errors)) {
+      this.props.onSubmitJog(params)
+      this.setState({ errors: {} })
+      this.refs.date.value = "";
+      this.refs.distance.value = "";
+      this.refs.duration.value = "";
+
+    } else {
+      this.setState({ errors: errors })
+    }
+  }
+
+  formattedDistance() {
+    let parsed = parseFloat(this.refs.distance.value) || 0
+    return parsed * 1000;
+  }
+
+  formattedDuration() {
+    let durationString = this.refs.duration.value;
+
+    if(durationString.match(/:/)) {
+      return parseInt(durationString, 10) || 0
+    } else {
+      let [seconds, minutes, hours] = durationString.split(":").reverse()
+      [seconds, minutes, hours] = [seconds, minutes, hours].map(n => parseInt(n, 10) || 0)
+      return seconds + minutes * 60 + hours * 60;
+    }
+  }
+
+  validate(params) {
+    let errors = {}
+    if(isEmpty(params.date)) {
+      errors["date"] = "Please enter a valid date"
+    }
+    if(params.duration <= 0) {
+      errors["duration"] = "Please enter a valid duration (bigger than 0)"
+    }
+    if(params.distance <= 0) {
+      errors["distance"] = "Please enter a valid distance (bigger than 0)"
+    }
+
+    return errors;
   }
 
   render() {
@@ -21,14 +73,17 @@ class NewJog extends React.Component {
             <div className={ styles.field }>
               <label>Date</label>
               <input ref="date" type="date" />
+              { this.errorsFor("date") }
             </div>
             <div className={ styles.field }>
               <label>Distance (in km)</label>
               <input ref="distance" type="number" min="0.1" step="0.01" />
+              { this.errorsFor("distance") }
             </div>
             <div className={ styles.field }>
               <label>Duration ([HH:]MM:SS)</label>
               <input ref="duration" type="text" placeholder="e.g. 30:00 (= 30 minutes)"/>
+              { this.errorsFor("distance") }
             </div>
           </div>
           <div className={ styles.submit }>
@@ -36,6 +91,14 @@ class NewJog extends React.Component {
             <button type="submit">Save Jog</button>
           </div>
         </form>
+      </div>
+    )
+  }
+
+  errorsFor(attribute) {
+    return (
+      <div className={ styles.error }>
+        { this.state.errors && this.state.errors[attribute] }
       </div>
     )
   }
