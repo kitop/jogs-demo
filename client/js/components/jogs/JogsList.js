c3c8338e0ec38d609320e6cc03a87d57af4d2444
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-08 17:51:55 +0100

    wip: grouping

--- client/js/components/jogs/JogsList.js
+++ client/js/components/jogs/JogsList.js
@@ -1,25 +1,77 @@
 import React from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
+import moment from "moment";
 import Jog from "./Jog";
 import styles from "./styles.scss";
 
 class JogsList extends React.Component {
 
   render() {
-    let children = "";
+    return (
+      <div className={ styles.list }>
+        <h2>Jogs</h2>
+        { this.renderGroups() }
+        { this.emptyMessage() }
+      </div>
+    )
+  }
+
+  renderGroups() {
+    if(!this.props.jogs) { return }
+
+    let minDate = _.minBy(this.props.jogs, jog => jog.date );
+    let maxDate = _.maxBy(this.props.jogs, jog => jog.date );
+
+    let grouped = _.groupBy(this.props.jogs, jog => {
+      let date = moment(jog.date);
+      return [date.year(), date.week()].join(" ");
+    });
+
+    // TODO sort groups
+
+    let renderedGroups = _.map(grouped, (value, key) => {
+      let list = _.chain(value).sortBy(jog => jog.date).reverse().value()
+
+      return this.renderGroup(key, list)
+    })
+
+    return renderedGroups;
+  }
+
+  renderGroup(week_key, jogs) {
+    let date = moment(week_key, "YYYY w");
+    let from = date.clone().startOf('week');
+    let to = date.endOf('week');
+    let distance = _.sumBy(jogs, jog => jog.distance);
+    let averageSpeed = _.sumBy(jogs, jog => jog.average_speed) / jogs.length
+
+    return (
+      <div key={week_key}>
+        <header>
+          <div>
+            { from.format("MMMM Do YYYY") } - { to.format("MMMM Do YYYY") }
+          </div>
+          <span>
+            Total Distance: { distance }m
+          </span>
+          <span>
+            Avg Speed { averageSpeed } m/s
+          </span>
+        </header>
+        { jogs.map(jog => this.renderJog(jog)) }
+      </div>
+    )
+  }
+
+  emptyMessage() {
+    let children;
     if(this.props.jogs == null) {
       children = <div className={ styles.loading }>Loading...</div>
     } else if(this.props.jogs.length == 0) {
       children = <div className={ styles.empty }>No jogs logged yet!</div>
-    } else {
-      children = this.props.jogs.map(jog => this.renderJog(jog))
     }
-    return (
-      <div className={ styles.list }>
-        <h2>Jogs</h2>
-        { children }
-      </div>
-    )
+    return children
   }
 
   renderJog(jog) {
