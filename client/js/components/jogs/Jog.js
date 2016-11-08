e18ac722a5d389babc093f6cd820365e03e23689
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-08 13:08:41 +0100

    validate on edit as well

--- client/js/components/jogs/Jog.js
+++ client/js/components/jogs/Jog.js
@@ -86,9 +86,8 @@ class Jog extends React.Component {
     let errors = {}
     if(isEmpty(params.date)) {
       errors["date"] = "Please enter a valid date"
-    }
-    if(params.duration <= 0) {
-      errors["duration"] = "Please enter a valid duration (bigger than 0)"
+    } else if(!( params.date.match(/\d{4}-\d{2}-\d{2}/) || params.date.match(/\d{2}\/\d{2}\/\d{4}/) )) {
+      errors["date"] = "Please enter a valid date (YYYY-MM-DD or DD/MM/YYYY)"
     }
     if(params.distance <= 0) {
       errors["distance"] = "Please enter a valid distance (bigger than 0)"
