4fd0ca7e16c0ffe59bc717db9ca9a143edd150ba
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-08 13:07:37 +0100

    properly validate date

--- client/js/components/jogs/NewJog.js
+++ client/js/components/jogs/NewJog.js
@@ -52,7 +52,10 @@ class NewJog extends React.Component {
     let errors = {}
     if(isEmpty(params.date)) {
       errors["date"] = "Please enter a valid date"
+    } else if(!( params.date.match(/\d{4}-\d{2}-\d{2}/) || params.date.match(/\d{2}\/\d{2}\/\d{4}/) )) {
+      errors["date"] = "Please enter a valid date (YYYY-MM-DD or DD/MM/YYYY)"
     }
+
     if(params.duration <= 0) {
       errors["duration"] = "Please enter a valid duration (bigger than 0)"
     }
