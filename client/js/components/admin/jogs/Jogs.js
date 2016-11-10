6df67e14be63ff11e267a0d1413f7b4a8309d6e5
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-10 18:39:11 +0100

    edit user jogs

--- client/js/components/admin/jogs/Jogs.js
+++ client/js/components/admin/jogs/Jogs.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router";
 import AdminLayout from "../Layout";
+import JogsContainer from "../../jogs/JogsContainer";
+import styles from "./styles.scss"
 
 class AdminJogs extends React.Component {
   render() {
+    let userId = this.props.params.userId;
     return (
       <AdminLayout>
-        Jogs
+        <header className={styles.headerMessage}>
+          <h2>
+            Editing jogs from User ID <b>{ userId }</b>
+          </h2>
+          <Link to="/admin">&lsaquo; Go Back</Link>
+        </header>
+        <JogsContainer
+          targetUserId={userId}
+        />
       </AdminLayout>
     )
   }
