11f3020b1cdef0eeabeb9f726aba2d4454e1a266
Esteban Pastorino <ejpastorino@gmail.com>
2016-11-13 18:06:09 +0100

    test jogs actions

--- client/test/store/jogs/action.spec.js
+++ client/test/store/jogs/action.spec.js
@@ -1,11 +1,9 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import moxios from 'moxios';
-import { push } from "react-router-redux";
 import expect from 'expect';
 import * as types from '../../../js/store/jogs/action_types';
 import * as actions from '../../../js/store/jogs/actions';
-import reducer from '../../../js/store/jogs/reducer';
 
 import xhr from '../../../js/utils/http_client';
 
@@ -45,4 +43,84 @@ describe("jogs actions", () => {
     })
   });
 
-})
+  it('creates ADD_JOG after creating a jog', () => {
+    const params = {
+      date: "2016-11-15",
+      duration: 3600,
+      distance: 10000,
+      average_speed: 2.77,
+    }
+    moxios.stubRequest(/\.*\/users\/1\/jogs/, {
+      status: 200,
+      response: {
+        ...params,
+        id: 1
+      }
+    })
+
+    const expectedActions = [
+      {
+        type: types.ADD_JOG,
+        id: 1,
+        ...params
+      }
+    ]
+    const store = mockStore()
+
+    store.dispatch(actions.createJog(1, params))
+    moxios.wait(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  });
+
+  it('creates EDIT_JOG after updating a jog', () => {
+    const params = {
+      date: "2016-11-15",
+      duration: 3600,
+      distance: 10000,
+      average_speed: 2.77,
+    }
+    moxios.stubRequest(/\.*\/users\/1\/jogs\/2/, {
+      status: 200,
+      response: {
+        ...params,
+        id: 2
+      }
+    })
+
+    const expectedActions = [
+      {
+        type: types.EDIT_JOG,
+        id: 2,
+        ...params
+      }
+    ]
+    const store = mockStore()
+
+    store.dispatch(actions.updateJog(1, 2, params))
+    moxios.wait(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  });
+
+  it('creates DELETE_JOG after deleting a jog', () => {
+    moxios.stubRequest(/\.*\/users\/1\/jogs\/2/, {
+      status: 204,
+      response: { }
+    })
+
+    const expectedActions = [
+      {
+        type: types.DELETE_JOG,
+        id: 2,
+      }
+    ]
+    const store = mockStore()
+
+    store.dispatch(actions.deleteJog(1, 2))
+    moxios.wait(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  });
+
+});
